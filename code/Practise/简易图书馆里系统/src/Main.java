c9a95e82cce67556daaa05b199b36cd52e4ca4f5
superhandsomechuan <781728963@qq.com>
2020-06-11 12:53:15 +0800

    1

--- code/Practise/简易图书馆里系统/src/Main.java
+++ code/Practise/简易图书馆里系统/src/Main.java
@@ -6,6 +6,7 @@
  */
 
 import book.BookList;
+import exception.Myexception;
 import user.Admin;
 import user.NormalUser;
 import user.User;
@@ -13,14 +14,24 @@ import user.User;
 import java.util.Scanner;
 
 public class Main {
-    public static User login(){
+    public static User login () throws Myexception
+    {
+        String password = "123456";
+
         Scanner scan = new Scanner(System.in);
         System.out.println("请输入用户姓名");
         String name = scan.nextLine();
         System.out.println("请输入选择：0 -> 管理员 1 -> 用户");
         int choice = scan.nextInt();
+        scan.nextLine(); // 读取输入 choice 时留下的换行符
 
         if(choice == 0){
+            System.out.println("请输入密码");
+            String passwd = scan.nextLine();
+
+            if(!passwd.equals(password))
+                throw new Myexception("wrong password");
+
             // 向上转型
             return new Admin(name);
         }else{
@@ -33,7 +44,12 @@ public class Main {
         BookList bookList = new BookList();
         while(true) {
             // 用户：
-            User user = login();
+            User user = null;
+            try {
+                user = login();
+            } catch (Myexception myexception) {
+                myexception.printStackTrace();
+            }
             // 应用运行时绑定，父类引用会使用子类的 menu 方法
             int choice = user.menu();
 
---
+++ code/Practise/简易图书馆里系统/src/exception/Myexception.java
@@ -0,0 +1,18 @@
+package exception;
+
+/*
+ *  Description:
+ *  User: Shepard Wang
+ *  Date: 2020 -06 -11
+ *  Time: 12:47
+ */
+
+public class Myexception extends Exception {
+    public Myexception() {
+        super();
+    }
+
+    public Myexception(String message) {
+        super(message);
+    }
+}
